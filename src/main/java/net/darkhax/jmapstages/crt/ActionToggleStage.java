4482818a18a178af5b1a314f5e72cee1202777de
Tyler Hancock <darklime@live.ca>
2018-03-08 18:04:12 -0700

    Formatted code.

--- src/main/java/net/darkhax/jmapstages/crt/ActionToggleStage.java
+++ src/main/java/net/darkhax/jmapstages/crt/ActionToggleStage.java
@@ -4,20 +4,20 @@ import crafttweaker.IAction;
 import net.darkhax.jmapstages.JMapStages;
 
 public class ActionToggleStage implements IAction {
-    
+
     private final Type type;
     private final String stage;
-    
+
     public ActionToggleStage (Type type, String stage) {
-        
+
         this.type = type;
         this.stage = stage;
     }
-    
+
     @Override
     public void apply () {
-        
-        switch (type) {
+
+        switch (this.type) {
             case DEATHPOINT:
                 JMapStages.stageDeathoint = this.stage;
                 break;
@@ -34,15 +34,15 @@ public class ActionToggleStage implements IAction {
                 break;
         }
     }
-    
+
     @Override
     public String describe () {
-        
+
         return String.format("Restricting Journey Map %s stage to %s.", this.type.name().toLowerCase(), this.stage);
     }
-    
+
     enum Type {
-        
+
         FULLSCREEN,
         MINIMAP,
         WAYPOINT,
